902a770d440426199cfbffd5136a1fbf0bf4def0
Doni Rubiagatra <doni@zero-one-group.com>
2023-04-28 09:59:36 +0700

    learn about C prompt

--- c/lispy/prompt.c
+++ c/lispy/prompt.c
@@ -1,17 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+void add_history(char* unused) {}
+
+
+
+#else
+#include <readline/readline.h>
+#include <readline/history.h>
+#endif
+
 
 int main(int argc, char** argv) {
-  puts("Fanly version 0.0.1");
+  puts("Lispy version 0.0.1");
   puts("Press Ctrl+c to Exit");
 
   while (1) {
-    fputs("fanli> ", stdout);
-    
-    fgets(input, 2048, stdin);
 
-    printf("No you're a %s", input);
+    char* input = readline("lispy> ");
+   
+    add_history(input);
+
+    printf("No you're a %s\n", input);
+
+    free(input);
   }
 
   return 0;
